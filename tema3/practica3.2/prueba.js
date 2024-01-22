20890f864fdd21d963b9b209e572823a981fa446
tania <taniavieco@gmail.com>
2024-01-22 13:41:10 +0100

    hoy

--- tema3/practica3.2/prueba.js
+++ tema3/practica3.2/prueba.js
@@ -1,23 +1,31 @@
 const audio = document.getElementById("myaudio");
-const volume =  document.getElementById("volumen").value;
+const volume = document.getElementById("volumen");
 const play = document.getElementById("playpause");
 
-
-
-function playaudio(){
-    
-    audio.play();  
-    play.src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAl0lEQVRIS2NkoDFgpLH5DKMWEAzhAQ+iDUAnFgDxA4JOxaGAkA/+A/V9AOIJQNxIjiXEWAAzF+SLRCA+QIpFpFgAMxcUbIXEBhs5FoAsIjrYyLWA6GCj1IKHQJsS8MULuRZ8hKasBkIRTo4FG0nJG6RYQDA4sPmGGAuIDg5yLKB5UUEoDgnKEwoiggYQUjBqAaEQon2lDwCCBSIZQeNI9AAAAABJRU5ErkJggg==";
-    
-    audio.pause(); 
-    play.src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAARElEQVRIS2NkoDFgpLH5DKMWEAzhkRFE/5HCAeZjbGJYg4uYIBq1gGBKGw2i0SDCXyITk9EIhiE+BaMWEAy+0SAiGEQAoSkUGZGNMAQAAAAASUVORK5CYII=";
-}   
-
-function restartaudio(){
-    audio.pause(); 
-    audio.currentTime = 0; 
-    audio.play();  
+function playaudio() {
+    let imgplay = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAl0lEQVRIS2NkoDFgpLH5DKMWEAzhAQ+iDUAnFgDxA4JOxaGAkA/+A/V9AOIJQNxIjiXEWAAzF+SLRCA+QIpFpFgAMxcUbIXEBhs5FoAsIjrYyLWA6GCj1IKHQJsS8MULuRZ8hKasBkIRTo4FG0nJG6RYQDA4sPmGGAuIDg5yLKB5UUEoDgnKEwoiggYQUjBqAaEQon2lDwCCBSIZQeNI9AAAAABJRU5ErkJggg==";
+    let imgpause = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAARElEQVRIS2NkoDFgpLH5DKMWEAzhkRFE/5HCAeZjbGJYg4uYIBq1gGBKGw2i0SDCXyITk9EIhiE+BaMWEAy+0SAiGEQAoSkUGZGNMAQAAAAASUVORK5CYII=";
+    if (play.src === imgplay) {
+        audio.play();
+        play.src = imgpause;
+    } else if (play.src === imgpause) {
+        audio.pause();
+        play.src = imgplay;
+    }
 }
-function volumen(){
-    audio.volumen = volume; 
+
+function restartaudio() {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
 }
+
+function volumen() {
+    audio.volume = volume.value / 100;
+    document.getElementById("volumenN").innerHTML = Math.round(audio.volume * 100) + "%";
+
+
+}
+
+
+
